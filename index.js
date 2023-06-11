15a5cc1807b816b6daf4a454438c640d8a1ed608
Monira Jahan <monirajahan459@gmail.com>
2023-06-12 00:49:26 +0600

    Reviews collection added

--- index.js
+++ index.js
@@ -30,9 +30,14 @@ async function run() {
     await client.connect();
 
     const instructorCollection = client.db("schoolDB").collection("instructors");
+    const reviewCollection = client.db("schoolDB").collection("reviews");
     
     app.get('/instructors',async(req,res)=>{
       const result=await instructorCollection.find().toArray();
+      res.send(result);
+    })
+    app.get('/reviews',async(req,res)=>{
+      const result=await reviewCollection.find().toArray();
       res.send(result);
     })
     // Send a ping to confirm a successful connection
